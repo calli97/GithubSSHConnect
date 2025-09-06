62515e61e38a97337200e9fc199af2f6e1f11e0c
Nahuel Callalli <nahuel@venturing.com.ar>
2025-09-06 15:10:08 -0300

    add quiet

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -2,5 +2,5 @@
 
 echo "Please enter your email:"
 read email
-echo "Hola $email - Empezando generacion de ssh"
-ssh-keygen -t ed25519 -C "${email}"
+echo "Hi $email - Generating ssh key..."
+ssh-keygen -t -q ed25519 -C "${email}"
