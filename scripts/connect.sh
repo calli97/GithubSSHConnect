52645609d6133185bbb0216ee3486d661e113b42
Nahuel Callalli <nahuel@venturing.com.ar>
2025-09-06 19:48:30 -0300

    remove old

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -11,13 +11,13 @@ username="${email%@*}"
 
 echo "USERNAME: $username\n"
 
-if [ "$user" = "root"];then
-    rootPath="~/root/.ssh/$username"
-else
-    rootPath="~/home/$user/.ssh/$username"
-fi
+# if [ "$user" = "root"];then
+#     rootPath="~/root/.ssh/$username"
+# else
+#     rootPath="~/home/$user/.ssh/$username"
+# fi
 
-echo "ROOTHPATH: $rootPath\n"
+# echo "ROOTHPATH: $rootPath\n"
 
-echo "Hi $email - Generating ssh key..."
-ssh-keygen -q -t ed25519 -C "${email}" -f "$rootPath" -N ""
+# echo "Hi $email - Generating ssh key..."
+# ssh-keygen -q -t ed25519 -C "${email}" -f "$rootPath" -N ""
