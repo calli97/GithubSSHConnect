3f09db73cd3c0148606687f9386dcf75ef33662b
Nahuel Callalli <nahuel@venturing.com.ar>
2025-09-06 19:53:54 -0300

    lin

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-
-user=${whoami}
-
-echo "USER: $user"
-
-echo "Please enter your email:"
-read email
-
-username="${email%@*}"
-
-echo "USERNAME: $username"
-
-# if [ "$user" = "root"];then
-#     rootPath="~/root/.ssh/$username"
-# else
-#     rootPath="~/home/$user/.ssh/$username"
-# fi
-
-# echo "ROOTHPATH: $rootPath\n"
-
-# echo "Hi $email - Generating ssh key..."
-# ssh-keygen -q -t ed25519 -C "${email}" -f "$rootPath" -N ""
+git add .
+g
