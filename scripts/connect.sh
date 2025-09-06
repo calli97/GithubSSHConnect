85126c66df9c11865a7a39cb8928b6712eace1e5
Nahuel Callalli <nahuel@venturing.com.ar>
2025-09-06 19:42:25 -0300

    only email need it

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+user=${whoami}
+
+
 echo "Please enter your email:"
 read email
+
+username="${email%@*}"
+
+if [ "$user" = "root"];then
+    rootPath="~/root/.ssh/$username"
+else
+    rootPath="~/home/$user/.ssh/$username"
+fi
+
 echo "Hi $email - Generating ssh key..."
-ssh-keygen -t ed25519 -C "${email}"
+ssh-keygen -q -t ed25519 -C "${email}" -f "$rootPath"
